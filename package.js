fe99f79c3e47b00e5cf56af344577db536576d50
Chris Oloff <christian.oloff@gmail.com>
2014-11-13 13:07:16 +0200

    filled in github url

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'chris2:oauth2-mitre',
   summary: 'Oauth2 flow for MitreID Connect',
   version: '0.0.1',
-  git: ' /* Fill me in! */ '
+  git: 'git@github.com:Chris927/chris2-oauth2-mitre.git'
 });
 
 Package.onUse(function(api) {
