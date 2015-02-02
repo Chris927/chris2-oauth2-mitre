25cc4f0e40d7e66f0b501cb95a83ff1e49944ce9
Chris Oloff <christian.oloff@gmail.com>
2015-02-02 16:54:13 +0200

    http call with refresh token support, cleanup required

--- mitre_server.js
+++ mitre_server.js
@@ -2,12 +2,13 @@ Mitre = {};
 
 OAuth.registerService('mitre', 2, null, function(query) {
 
-  var accessToken = getAccessToken(query);
-  var identity = getIdentity(accessToken);
+  var tokens = getToken(query);
+  var identity = getIdentity(tokens.access_token);
 
   var serviceData = {
     id: identity.sub,
-    accessToken: OAuth.sealSecret(accessToken),
+    accessToken: OAuth.sealSecret(tokens.access_token),
+    refreshToken: OAuth.sealSecret(tokens.refresh_token),
     email: identity.email,
     username: identity.preferred_username
   };
@@ -32,7 +33,7 @@ var getConfig = function() {
   return config;
 };
 
-var getAccessToken = function (query) {
+var getToken = function (query) {
   var config = getConfig();
 
   var response;
@@ -52,6 +53,7 @@ var getAccessToken = function (query) {
           state: query.state
         }
       });
+    console.log('token response', response);
   } catch (err) {
     throw _.extend(new Error("Failed to complete OAuth handshake with Mitre. " + err.message),
                    {response: err.response});
@@ -59,7 +61,10 @@ var getAccessToken = function (query) {
   if (response.data.error) { // if the http response was a json object with an error attribute
     throw new Error("Failed to complete OAuth handshake with Mitre. " + response.data.error);
   } else {
-    return response.data.access_token;
+    return {
+      access_token: response.data.access_token,
+      refresh_token: response.data.refresh_token
+    };
   }
 };
 
@@ -81,3 +86,111 @@ var getIdentity = function (accessToken) {
 Mitre.retrieveCredential = function(credentialToken, credentialSecret) {
   return OAuth.retrieveCredential(credentialToken, credentialSecret);
 };
+
+Mitre.http = {};
+
+var getMeteorUser = function(userId) {
+  var user = Meteor.users.findOne(userId);
+  if (!user) throw new Error('user not found: ' + userId);
+  return user;
+}
+
+var getAccessTokenOfUser = function(userId) {
+  var user = getMeteorUser(userId);
+  return user.services.mitre.accessToken;
+}
+
+var getRefreshTokenOfUser = function(userId) {
+  var user = getMeteorUser(userId);
+  return OAuth.openSecret(user.services.mitre.refreshToken);
+}
+
+var getClientToken = function() {
+  var config = getConfig();
+  console.log('getting client_credentials...');
+  var result = HTTP.post(config.issuer + '/authorize', {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    content: 'grant_type=client_credentials' +
+      '&client_id=' + config.clientId +
+      '&client_secret=' + OAuth.openSecret(config.secret)
+  });
+  console.log('getting client_credentials, result', result);
+  return result;
+}
+
+var getNewAccessTokenViaRefresh = function(userId) {
+  var config = getConfig();
+  try {
+    // var clientToken = getClientToken(); // TODO: unnecessary?
+    var body = 'grant_type=refresh_token' +
+        '&refresh_token=' + getRefreshTokenOfUser(userId) +
+        '&client_id=' + config.clientId +
+        '&client_secret=' + OAuth.openSecret(config.secret);
+    console.log('getNewAccessTokenViaRefresh, body', body);
+    var response = HTTP.post(
+      config.issuer + "/token", {
+      headers: {
+        Accept: 'application/json',
+        'User-Agent': userAgent,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      content: body
+    });
+    console.log('via refresh, response', response);
+    return response.data && {
+      access_token: response.data.access_token,
+      refresh_token: response.data.refresh_token
+    };
+  } catch (err) {
+    throw _.extend(new Error("Failed to refresh token from Mitre. " + err.message),
+                   {response: err.response});
+  }
+  if (response.data.error) { // if the http response was a json object with an error attribute
+    throw new Error("Failed to refresh token from Mitre: " + response.data.error);
+  } else {
+    return {
+      access_token: response.data.access_token,
+      refresh_token: response.data.refresh_token
+    };
+  }
+}
+
+var storeNewAccessToken = function(userId, newAccessToken, newRefreshToken) {
+  console.log('should store new access token', newAccessToken);
+  Meteor.users.update({ _id: userId }, {
+    $set: {
+      'services.mitre.accessToken': newAccessToken,
+      'services.mitre.refreshToken': newRefreshToken
+    }
+  });
+}
+
+Mitre.http.call = function(userId, method, url, options) {
+  var result;
+
+  try {
+    result = doCall();
+  } catch (e) {
+    console.log('exception while doCall', e);
+    if (e.response && e.response.statusCode === 401) {
+      var tokens = getNewAccessTokenViaRefresh(userId);
+      if (tokens) {
+        storeNewAccessToken(userId, tokens.access_token, tokens.refresh_token);
+        return doCall();
+      }
+    }
+  }
+
+  function doCall() {
+    console.log('should call:', userId, method, url, options);
+    var extendedOptions = _.clone(options) || {};
+    extendedOptions.headers = _.extend(extendedOptions.headers || {}, {
+      Authorization: 'Bearer ' + getAccessTokenOfUser(userId)
+    })
+    return Meteor.http.call(method, url, extendedOptions);
+  }
+
+  return result;
+}
